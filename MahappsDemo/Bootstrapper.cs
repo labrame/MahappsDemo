59f7a5b7968887720dd515902b8bf01cf4291d15
labrame <labrame.blog@gmail.com>
2016-02-12 20:22:00 +0000

    Integration of caliburn.micro to the solution

---
+++ MahappsDemo/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahappsDemo
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ MahappsDemo/ShellViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MahappsDemo
+{
+    public class ShellViewModel : PropertyChangedBase  
+    { 
+        string _name ;
+
+        public string Name { 
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}", Name));
+        }
+
+    }
+}
