631e844d7fc0ce1f3e0b47b610265b644c5f2919
labrame <labrame.blog@gmail.com>
2016-02-23 06:58:05 +0000

    add constant

--- MahappsDemo/ViewModel/FretBoardTwoViewModel.cs
+++ MahappsDemo/ViewModel/FretBoardTwoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using MahappsDemo.Model;
+using System;
 using System.Windows;
 using Enum = MahappsDemo.Model.Enum;
 
@@ -8,6 +9,9 @@ namespace MahappsDemo.ViewModel
     public class FretBoardTwoViewModel : BaseTabViewModel
     {
         public readonly string[] _scale;
+        public readonly string[] _string;
+        public readonly Random _random;
+        public readonly string _template;
 
         public BindableCollection<Fret> EString { get; set; }
         public BindableCollection<Fret> AString { get; set; }
@@ -28,6 +32,8 @@ namespace MahappsDemo.ViewModel
             E2String = CreateString(Enum.Standard.e);
 
             _scale = new string[] { "A", "A#Bb", "B", "C", "C#Db", "D", "D#Eb", "E", "F", "F#Gb", "G", "G#Ab" };
+            _string = new string[] { "E", "A", "D", "G", "B", "e" };
+            _random = new Random();
 
             IsAnswerVisible = false;
             CreateNewQuestion();
@@ -37,23 +43,6 @@ namespace MahappsDemo.ViewModel
         {
             MessageBox.Show(string.Format("Just fretted position {0} on string {1}", 
                 fret.Position, fret.StringName));
-        }
-
-        private BindableCollection<Fret> CreateString(Enum.Standard stringName)
-        {
-            var stringModel = new BindableCollection<Fret>();
-
-            for (int i = 0; i < 12; i++)
-            {
-                stringModel.Add(new Fret() { StringName = stringName, Position = i });
-            }
-
-            return stringModel;
-        }
-
-        private void CreateNewQuestion()
-        {
-            Question = "This is the question";
         }
 
         private string _question;
@@ -89,6 +78,31 @@ namespace MahappsDemo.ViewModel
             }
         }
 
+        private BindableCollection<Fret> CreateString(Enum.Standard stringName)
+        {
+            var stringModel = new BindableCollection<Fret>();
 
+            for (int i = 0; i < 12; i++)
+            {
+                stringModel.Add(new Fret() { StringName = stringName, Position = i });
+            }
+
+            return stringModel;
+        }
+
+        private void CreateNewQuestion()
+        {
+            Question = "This is the question";
+        }
+
+        private string RandomString()
+        {
+            return _string[_random.Next(0, 5)];
+        }
+
+        private string RandomNote()
+        {
+            return _scale[_random.Next(0, 11)];
+        }
     }
 }
